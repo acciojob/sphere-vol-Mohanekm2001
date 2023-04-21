33509e4fbd93fe7df77e33af78f2da19f8b0c646
Mohanekm2001 <mohan2001ekm@gmail.com>
2023-04-21 09:16:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function volume_sphere() {
     //Write your code here
-  
+	var rad = document.getElementById("radius").value;
+	rad = Math.abs(rad);
+	var val = (4/3) * Math.PI * Math.pow(rad, 3);
+	val = val.toFixed(4);
+	document.getElementById("volume").value = val;
+	return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
